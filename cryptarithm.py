ab3cc4c0ee6c81ab56bca5cb5509ac941eac14f8
Poonuri Hansika Reddy <poonurihansikareddy@gmail.com>
2026-04-01 23:53:20 +0530

    Add files via upload

---
+++ cryptarithm.py
@@ -0,0 +1,18 @@
+import itertools
+
+letters = 'SENDMORY'
+digits = range(10)
+
+for perm in itertools.permutations(digits, len(letters)):
+    mapping = dict(zip(letters, perm))
+
+    if mapping['S'] == 0 or mapping['M'] == 0:
+        continue
+
+    send = 1000*mapping['S'] + 100*mapping['E'] + 10*mapping['N'] + mapping['D']
+    more = 1000*mapping['M'] + 100*mapping['O'] + 10*mapping['R'] + mapping['E']
+    money = 10000*mapping['M'] + 1000*mapping['O'] + 100*mapping['N'] + 10*mapping['E'] + mapping['Y']
+
+    if send + more == money:
+        print(mapping)
+        break
